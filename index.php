5410a4678fa645cc5f73554bf9044f6108612bdb
Umesh Patel <umesh.patel@live.co.uk>
2018-01-09 22:24:49 +0000

    added initial files

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en-gb">
+<head>
+	<meta charset="utf-8">
+	<title>Room Four Dessert | Home</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!--[if lt IE 9]>
+		<script src="assets/js/html5shiv.min.js"></script>
+	<![endif]-->
+	<link rel="icon" type="image/x-icon" href="assets/images/favicon.ico">
+	<link rel="stylesheet" href="assets/css/style.css"> 
+</head>
+
+<body>
+	<div class="container">
+		<header role="banner">
+			<p class="sr-only sr-only-focusable"><a href="#content" title="Go to the main content">Skip to content</a></p>
+			<nav></nav>
+		</header>
+
+		<main id="content" role="main">	
+			
+		</main>
+		
+		<footer role="contentinfo">
+			<p class="copyright">&copy;Copyright Room Four Dessert <?php echo gmdate("Y"); ?>. All rights reserved.</p>
+		</footer>
+	</div>
+</body>
+</html>
