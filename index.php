a7435ea5d03ce3e3d915f0cff676ef8e80a22d5a
Umesh Patel <umesh.patel@live.co.uk>
2018-01-10 16:49:37 +0000

    added written content

--- index.php
+++ index.php
@@ -30,12 +30,32 @@
 		</header>
 
 		<main id="content" role="main">	
-			<section class="intro"></section>
-			<section class="about"></section>
-			<section class="menu"></section>
-			<section class="catering"></section>
-			<section class="gallery"></section>
-			<section class="contact"></section>
+			<section class="intro">
+				<h1>Welcome to Room Four Dessert</h1>
+				<p>No matter what you've eaten, there is always room for dessert</p>
+			</section>
+			<section class="about">
+				<h2>About Us</h2>
+				<p>Established in 2013, we have secured ourselves firmly on the map as Bolton's premier dessert house.</p>
+				<p>Whet your appetite with our extensive menu and visit us to see what extras are on offer in our evolving display fridge. We can assure you that you will be spoilt for choice.</p>
+				<p>We offer something for every dessert aficionado alongside a finely honed breakfast and lunch menu to offer something to precede the most important part of a meal, anytime of the day.</p>
+			</section>
+			<section class="menu">
+				<h2>Our Menus</h2>
+				<p>It’s easy to decide you want to eat out. It's harder to find somewhere that'll be able to satisfy your cravings. Our menu will keep you coming back for more. Do remember to leave Room Four Dessert though!</p>
+				<a href="">View Menu</a>
+			</section>
+			<section class="catering">
+				<h2>Event Catering</h2>
+				<p>Let us do the hard work, impress your guests with a stylish and tasty winning combination. Catering fit for any occasion. You can tailor it to however specific your requirments/tastes.</p>
+			</section>
+			<section class="gallery">
+				<h2>Gallery</h2>
+				<p>Send us your pictures by email, or via our social media pages to feature on the gallery page. Every month, the featured picture will win a prize.</p>
+			</section>
+			<section class="contact">
+				<h2>Contact Us</h2>
+			</section>
 		</main>
 		
 		<footer role="contentinfo">
