3e0a1ba81130e36a76847766bd78aa4aa093188c
Umesh Patel <umesh.patel@live.co.uk>
2018-01-10 20:41:46 +0000

    edited headings and added business info structure

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
 					<li><a href="">Menu</a></li>
 					<li><a href="">Catering</a></li>
 					<li><a href="">Gallery</a></li>
-					<li><a href="">Contact Us</a></li>
+					<li><a href="">Contact us</a></li>
 				</ul>
 			</nav>
 		</header>
@@ -35,18 +35,18 @@
 				<p>No matter what you've eaten, there is always room for dessert</p>
 			</section>
 			<section class="about">
-				<h2>About Us</h2>
+				<h2>About us</h2>
 				<p>Established in 2013, we have secured ourselves firmly on the map as Bolton's premier dessert house.</p>
 				<p>Whet your appetite with our extensive menu and visit us to see what extras are on offer in our evolving display fridge. We can assure you that you will be spoilt for choice.</p>
 				<p>We offer something for every dessert aficionado alongside a finely honed breakfast and lunch menu to offer something to precede the most important part of a meal, anytime of the day.</p>
 			</section>
 			<section class="menu">
-				<h2>Our Menus</h2>
+				<h2>Our menus</h2>
 				<p>It’s easy to decide you want to eat out. It's harder to find somewhere that'll be able to satisfy your cravings. Our menu will keep you coming back for more. Do remember to leave Room Four Dessert though!</p>
 				<a href="">View Menu</a>
 			</section>
 			<section class="catering">
-				<h2>Event Catering</h2>
+				<h2>Event catering</h2>
 				<p>Let us do the hard work, impress your guests with a stylish and tasty winning combination. Catering fit for any occasion. You can tailor it to however specific your requirments/tastes.</p>
 			</section>
 			<section class="gallery">
@@ -59,13 +59,26 @@
 		</main>
 		
 		<footer role="contentinfo">
+			<div class="info">
+				<ul>
+					<li class="location">
+						<h2>Find us</h2>
+						<!-- Address -->
+					</li>
+					<li class="hours">
+						<h2>Our business hours</h2>
+						<!-- Business hours -->
+					</li>
+					<li class="social">
+						<h2>Get social with us</h2>
+						<!-- Facebook -->
+						<!-- Twitter -->
+						<!-- Instagram -->
+					</li>
+				</ul>
+			</div>
 			<div class="copyright">
 				<p>&copy;Copyright Room Four Dessert <?php echo gmdate("Y"); ?>. All rights reserved.</p>
-			</div>
-			<div class="social">
-				<!-- Facebook -->
-				<!-- Twitter -->
-				<!-- Instagram -->
 			</div>
 		</footer>
 	</div>
