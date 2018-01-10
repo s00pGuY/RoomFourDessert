af01bf6b1c19dede1910acde37d618087da9f72d
Umesh Patel <umesh.patel@live.co.uk>
2018-01-10 22:55:13 +0000

    added business hours

--- index.php
+++ index.php
@@ -69,7 +69,15 @@
 					</li>
 					<li class="hours">
 						<h2>Our business hours</h2>
-						<!-- Business hours -->
+						<ul>
+							<li><p>Monday <span>09:00 - 23:00</span></p></li>
+							<li><p>Tuesday <span>09:00 - 23:00</span></p></li>
+							<li><p>Wednesday <span>09:00 - 23:00</span></p></li>
+							<li><p>Thursday <span>09:00 - 23:00</span></p></li>
+							<li><p>Friday <span>09:00 - 00:00</span></p></li>
+							<li><p>Saturday <span>10:00 - 00:00</span></p></li>
+							<li><p>Sunday <span>10:00 - 23:00</span></p></li>
+						</ul>
 					</li>
 					<li class="social">
 						<h2>Get social with us</h2>
