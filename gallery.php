a693b334a034bad67557c1332db6543b85ce8b02
Umesh Patel <umesh.patel@live.co.uk>
2018-01-13 17:18:14 +0000

    initialised gallery page

---
+++ gallery.php
@@ -0,0 +1,14 @@
+<?php 
+include_once("header.php");
+$page_title = 'Gallery - Room Four Dessert';
+$page_desc = '';
+$page_keywords = '';
+?>
+
+		<main id="content" role="main">
+			<section class="intro">
+				<h1>Gallery</h1>
+			</section>
+		</main>
+
+<?php include_once("footer.php"); ?>
