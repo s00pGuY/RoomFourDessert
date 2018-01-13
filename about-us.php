869279d558a01921afa743cdf77dffa5298bef03
Umesh Patel <umesh.patel@live.co.uk>
2018-01-13 18:05:45 +0000

    initialised about us page

---
+++ about-us.php
@@ -0,0 +1,14 @@
+<?php 
+include_once("header.php");
+$page_title = 'About us - Room Four Dessert';
+$page_desc = '';
+$page_keywords = '';
+?>
+
+		<main id="content" role="main">
+			<section class="intro">
+				<h1>About us</h1>
+			</section>
+		</main>
+
+<?php include_once("footer.php"); ?>
