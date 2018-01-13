4864c66ca125e39674b53418855f86e466fabadb
Umesh Patel <umesh.patel@live.co.uk>
2018-01-13 18:04:37 +0000

    initialised menu page

---
+++ menu.php
@@ -0,0 +1,14 @@
+<?php 
+include_once("header.php");
+$page_title = 'Menu - Room Four Dessert';
+$page_desc = '';
+$page_keywords = '';
+?>
+
+		<main id="content" role="main">
+			<section class="intro">
+				<h1>Menu</h1>
+			</section>
+		</main>
+
+<?php include_once("footer.php"); ?>
