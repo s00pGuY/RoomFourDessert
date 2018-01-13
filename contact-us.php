c073ea92ecd69d5ad1d520e69bfd667ad85e9501
Umesh Patel <umesh.patel@live.co.uk>
2018-01-13 17:48:12 +0000

    initialised contact page

---
+++ contact-us.php
@@ -0,0 +1,14 @@
+<?php 
+include_once("header.php");
+$page_title = 'Contact us - Room Four Dessert';
+$page_desc = '';
+$page_keywords = '';
+?>
+
+		<main id="content" role="main">
+			<section class="intro">
+				<h1>Contact us</h1>
+			</section>
+		</main>
+
+<?php include_once("footer.php"); ?>
