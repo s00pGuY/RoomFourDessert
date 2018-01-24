06e1ba31acd9034ead51ea7dd5b4e6d4c38abca1
Umesh Patel <umesh.patel@live.co.uk>
2018-01-24 18:12:56 +0000

    added flex wrapper to allow content to be placed in center

--- index.php
+++ index.php
@@ -7,33 +7,43 @@ include_once("header.php");
 
 		<main id="content" role="main">	
 			<section class="intro home">
-				<div class="slogan">
+				<div class="slogan flex-wrapper">
 					<p>No matter what you've eaten, there is always </p>
 					<h1>Room Four Dessert</h1>
 				</div>
 			</section>
 			<section class="about full-scn">
-				<h2>About us</h2>
-				<p>Established in 2013, we have secured ourselves firmly on the map as Bolton's premier dessert house.</p>
-				<p>Whet your appetite with our extensive menu and visit us to see what extras are on offer in our evolving display fridge. We can assure you that you will be spoilt for choice.</p>
-				<p>We offer something for every dessert aficionado alongside a finely honed breakfast and lunch menu to offer something to precede the most important part of a meal, anytime of the day.</p>
-				<a href="about-us.php" class="btn">Read more</a>
+				<div class="flex-wrapper">
+					<h2>About us</h2>
+					<p>Established in 2013, we have secured ourselves firmly on the map as Bolton's premier dessert house.</p>
+					<p>Whet your appetite with our extensive menu and visit us to see what extras are on offer in our evolving display fridge. We can assure you that you will be spoilt for choice.</p>
+					<p>We offer something for every dessert aficionado alongside a finely honed breakfast and lunch menu to offer something to precede the most important part of a meal, anytime of the day.</p>
+					<a href="about-us.php" class="btn">Read more</a>
+				</div>
 			</section>
 			<section class="menus full-scn">
-				<h2>Our menus</h2>
-				<p>It’s easy to decide you want to eat out. It's harder to find somewhere that'll be able to satisfy your cravings. Our menu will keep you coming back for more. Do remember to leave Room Four Dessert though!</p>
-				<a href="menu.php" class="btn">View Menu</a>
+				<div class="flex-wrapper">
+					<h2>Our menus</h2>
+					<p>It’s easy to decide you want to eat out. It's harder to find somewhere that'll be able to satisfy your cravings. Our menu will keep you coming back for more. Do remember to leave Room Four Dessert though!</p>
+					<a href="menu.php" class="btn">View Menu</a>
+				</div>
 			</section>
 			<section class="catering full-scn">
-				<h2>Event catering</h2>
-				<p>Let us do the hard work, impress your guests with a stylish and tasty winning combination. Catering fit for any occasion. You can tailor it to however specific your requirments/tastes.</p>
+				<div class="flex-wrapper">
+					<h2>Event catering</h2>
+					<p>Let us do the hard work, impress your guests with a stylish and tasty winning combination. Catering fit for any occasion. You can tailor it to however specific your requirments/tastes.</p>
+				</div>
 			</section>
 			<section class="img-gallery full-scn">
-				<h2>Gallery</h2>
-				<p>Send us your pictures by email, or via our social media pages to feature on the gallery page. Every month, the featured picture will win a prize.</p>
+				<div class="flex-wrapper">
+					<h2>Gallery</h2>
+					<p>Send us your pictures by email, or via our social media pages to feature on the gallery page. Every month, the featured picture will win a prize.</p>
+				</div>
 			</section>
 			<section class="contact full-scn">
-				<h2>Contact Us</h2>
+				<div class="flex-wrapper">
+					<h2>Contact Us</h2>
+				</div>
 			</section>
 		</main>
 
