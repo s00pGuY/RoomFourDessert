3047cc55c70cc0b234a559a1c050d18a6cbd7b3d
Umesh Patel <umesh.patel@live.co.uk>
2018-01-24 03:32:36 +0000

    added new class to make the relevant sections full screen

--- index.php
+++ index.php
@@ -10,26 +10,26 @@ include_once("header.php");
 				<h1>Welcome to Room Four Dessert</h1>
 				<p>No matter what you've eaten, there is always room for dessert</p>
 			</section>
-			<section class="about">
+			<section class="about full-scn">
 				<h2>About us</h2>
 				<p>Established in 2013, we have secured ourselves firmly on the map as Bolton's premier dessert house.</p>
 				<p>Whet your appetite with our extensive menu and visit us to see what extras are on offer in our evolving display fridge. We can assure you that you will be spoilt for choice.</p>
 				<p>We offer something for every dessert aficionado alongside a finely honed breakfast and lunch menu to offer something to precede the most important part of a meal, anytime of the day.</p>
 			</section>
-			<section class="menu">
+			<section class="menu full-scn">
 				<h2>Our menus</h2>
 				<p>It’s easy to decide you want to eat out. It's harder to find somewhere that'll be able to satisfy your cravings. Our menu will keep you coming back for more. Do remember to leave Room Four Dessert though!</p>
 				<a href="">View Menu</a>
 			</section>
-			<section class="catering">
+			<section class="catering full-scn">
 				<h2>Event catering</h2>
 				<p>Let us do the hard work, impress your guests with a stylish and tasty winning combination. Catering fit for any occasion. You can tailor it to however specific your requirments/tastes.</p>
 			</section>
-			<section class="gallery">
+			<section class="gallery full-scn">
 				<h2>Gallery</h2>
 				<p>Send us your pictures by email, or via our social media pages to feature on the gallery page. Every month, the featured picture will win a prize.</p>
 			</section>
-			<section class="contact">
+			<section class="contact full-scn">
 				<h2>Contact Us</h2>
 			</section>
 		</main>
