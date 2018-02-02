d107409965078452c659f7d73f32e2715917f5c0
Umesh Patel <umesh.patel@live.co.uk>
2018-02-02 22:00:13 +0000

    added contact form structure

--- contact-us.php
+++ contact-us.php
@@ -10,7 +10,28 @@ include_once("header.php");
 				<h1>Contact us</h1>
 			</section>
 			<section class="contact-form">
-				<!-- Contact Form -->
+				<h2>Send us a message</h2>
+				<form action="">
+					<p>
+						<label for="name">Name</label>
+						<input type="text" name="realname" id="name" class="formInput">
+					</p>
+					<p>
+						<label for="email">Email</label>
+						<input type="email" name="email" id="email" class="formInput">
+					</p>
+					<p>
+						<label for="phone">Telephone</label>
+						<input type="text" name="subject" id="phone" class="formInput">
+					</p>
+					<p>
+						<label for="msg">Message</label>
+						<textarea name="message" id="msg" class="formInput"></textarea>
+					</p>
+					<p>
+						<input type="submit" value="Send" class="btn">
+					</p>
+				</form>
 			</section>
 			<section class="find-us">
 				<h2>Find us</h2>
