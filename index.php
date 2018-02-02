79ded4123aeeb41b666929530cb788ee62157a11
Umesh Patel <umesh.patel@live.co.uk>
2018-02-02 02:24:31 +0000

    removed paragraph from about us section

--- index.php
+++ index.php
@@ -17,7 +17,6 @@ include_once("header.php");
 					<h2>About us</h2>
 					<p>Established in 2013, we have secured ourselves firmly on the map as Bolton's premier dessert house.</p>
 					<p>Whet your appetite with our extensive menu and visit us to see what extras are on offer in our evolving display fridge. We can assure you that you will be spoilt for choice.</p>
-					<p>We offer something for every dessert aficionado alongside a finely honed breakfast and lunch menu to offer something to precede the most important part of a meal, anytime of the day.</p>
 				</div>
 			</section>
 			<section class="menus full-scn">
