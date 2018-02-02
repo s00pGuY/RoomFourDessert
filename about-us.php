a4c5473aa5c6538453329c8299983de38865e772
Umesh Patel <umesh.patel@live.co.uk>
2018-02-02 02:34:56 +0000

    added content for about us page

--- about-us.php
+++ about-us.php
@@ -9,6 +9,18 @@ include_once("header.php");
 			<section class="intro about-us half-scn">
 				<h1>About us</h1>
 			</section>
+			<section class="about">
+				<h2>Room Four Dessert</h2>
+				<p>We offer something for every dessert aficionado alongside a finely honed breakfast and lunch menu to offer something to precede the most important part of a meal, anytime of the day.</p>
+			</section>
+			<section class="movenpick">
+				<h2>Movenpick ice cream</h2>
+				<p>We are proud to be official stockists of this luxury gourmet ice cream which is a multiple recipient of the "Cool Brands Award".</p>
+				<p>We serve only Movenpick with all of our desserts, guaranteeing you the very best quality ice cream with every indulgent Room Four Dessert moment</p>
+			</section>
+			<section class="awards">
+				
+			</section>
 		</main>
 
 <?php include_once("footer.php"); ?>
