9bbae75b3c73a081dbfdf196fa0c5599e51059cf
Umesh Patel <umesh.patel@live.co.uk>
2018-02-05 03:49:48 +0000

    added image on about section and merged menus and catering sections together

--- index.php
+++ index.php
@@ -17,15 +17,14 @@ include_once("header.php");
 				<p>Established in 2013, we have secured ourselves firmly on the map as Bolton's premier dessert house.</p>
 				<p>Whet your appetite with our extensive menu and visit us to see what extras are on offer in our evolving display fridge. We can assure you that you will be spoilt for choice.</p>
 				<p>We offer something for every dessert aficionado alongside a finely honed breakfast and lunch menu to offer something to precede the most important part of a meal, anytime of the day.</p>
+				<img src="assets/images/bg-images/showbox.png" width="455" height="300" alt=""> 
 			</section>
-			<section class="menus full-scn">
+			<section class="food full-scn">
 				<div class="flex-wrapper">
 					<h2>Our menus</h2>
 					<p>It’s easy to decide you want to eat out. It's harder to find somewhere that'll be able to satisfy your cravings. Our menu will keep you coming back for more. Do remember to leave Room Four Dessert though!</p>
 					<a href="menu.php" class="btn">View Menu</a>
 				</div>
-			</section>
-			<section class="catering full-scn">
 				<div class="flex-wrapper">
 					<h2>Event catering</h2>
 					<p>Let us do the hard work, impress your guests with a stylish and tasty winning combination. Catering fit for any occasion. You can tailor it to however specific your requirments/tastes.</p>
