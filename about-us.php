fb68fd4157b0d3c18e8f7c08d498e878d135f64f
Umesh Patel <umesh.patel@live.co.uk>
2018-02-12 02:43:33 +0000

    updated structure to work with new classes

--- about-us.php
+++ about-us.php
@@ -26,8 +26,8 @@ include_once("header.php");
 				<div class="wrapper flex">
 					<div class="split">
 						<h2>A little more about us</h2>
-						<p>We specialise in creating bespoke shot desserts with the sole aim of allowing you to flavour more than one dessert at a time and give you the choice of how much or how little you can enjoy. Quality is of paramount importance to us and we want you to be tantalised by our selection. Room Four Dessert allows you to enjoy the experience in the comfort of our relaxing and inviting shop on Deane Rd, Bolton or opt for the take away option and savour our selection in the comfort of your own home.</p>
-						<p>However it’s not just desserts that we specialise in. As a coffee shop and deli we have something to cater for everybody, from premium espresso based coffees made by trained baristas using locally roasted fresh coffee beans to a selection of hot beverages which will cater for all needs. As to our breakfast and lunch menus, we believe we have all bases covered. All we ask is that you do not deny the disputed fact that you will always have room for dessert.</p>
+						<p>We specialise in creating bespoke shot desserts with the sole aim of allowing you to flavour more than one dessert at a time and give you the choice of how much or how little you can enjoy. Quality is of paramount importance to us and we want you to be tantalised by our selection. Room Four Dessert allows you to enjoy the experience in the comfort of our relaxing and inviting shop on Deane Rd, Bolton or opt for the take away option and savour our selection in the comfort of your own home.<br>
+						However it’s not just desserts that we specialise in. As a coffee shop and deli we have something to cater for everybody, from premium espresso based coffees made by trained baristas using locally roasted fresh coffee beans to a selection of hot beverages which will cater for all needs. As to our breakfast and lunch menus, we believe we have all bases covered. All we ask is that you do not deny the disputed fact that you will always have room for dessert.</p>
 					</div>
 				</div>
 			</section>
--- contact-us.php
+++ contact-us.php
@@ -6,36 +6,41 @@ include_once("header.php");
 ?>
 
 		<main id="content" role="main">
-			<section class="intro contact half-scn">
-				<h1>Contact us</h1>
-			</section>
-			<section class="map">
-				<h2>Visit us</h2>
-				<!-- Interactive Map of location -->
+			<section class="intro contact flex hero">
+				<div class="wrapper">
+					<h1>Contact us</h1>
+				</div>
 			</section>
 			<section class="contact-form">
-				<h2>Send us a message</h2>
-				<form action="#">
-					<p>
-						<label for="name">Name</label>
-						<input type="text" name="realname" id="name" class="formInput">
-					</p>
-					<p>
-						<label for="email">Email</label>
-						<input type="email" name="email" id="email" class="formInput">
-					</p>
-					<p>
-						<label for="phone">Telephone</label>
-						<input type="text" name="subject" id="phone" class="formInput">
-					</p>
-					<p>
-						<label for="msg">Message</label>
-						<textarea name="message" id="msg" class="formInput"></textarea>
-					</p>
-					<p>
-						<input type="submit" value="Send" class="btn">
-					</p>
-				</form>
+				<div class="wrapper">
+					<h2>Send us a message</h2>
+					<form action="#">
+						<p>
+							<label for="name">Name</label>
+							<input type="text" name="realname" id="name" class="formInput">
+						</p>
+						<p>
+							<label for="email">Email</label>
+							<input type="email" name="email" id="email" class="formInput">
+						</p>
+						<p>
+							<label for="phone">Telephone</label>
+							<input type="text" name="subject" id="phone" class="formInput">
+						</p>
+						<p>
+							<label for="msg">Message</label>
+							<textarea name="message" id="msg" class="formInput"></textarea>
+						</p>
+						<p>
+							<input type="submit" value="Send" class="btn">
+						</p>
+					</form>
+				</div>
+			</section>
+			<section class="map">
+				<div class="wrapper">
+					<!-- Interactive Map of location -->
+				</div>
 			</section>
 		</main>
 
