a059ee647e68ac871b96911246325922349da7bd
Umesh Patel <umesh.patel@live.co.uk>
2018-02-12 02:11:01 +0000

    adapted to new classes and removed movenpick section

--- about-us.php
+++ about-us.php
@@ -6,22 +6,30 @@ include_once("header.php");
 ?>
 
 		<main id="content" role="main">
-			<section class="intro about half-scn">
-				<h1>About us</h1>
+			<section class="intro about flex hero">
+				<div class="wrapper">
+					<h1>About us</h1>
+				</div>
 			</section>
 			<section class="about-us">
-				<h2>Room Four Dessert</h2>
-				<p>We offer an indulgent range of desserts ranging from the old school custard based desserts for traditionalists to rich and luxurious treats for the dessert aficionado. We also offer freshly made waffles/crepes/pancakes and hot cookie dough served with Movenpick’s finest gourmet ice-cream.</p>
-				<p>Furthermore, we specialise in creating bespoke shot desserts with the sole aim of allowing you to flavour more than one dessert at a time and give you the choice of how much or how little you can enjoy. Quality is of paramount importance to us and we want you to be tantalised by our selection. Room Four Dessert allows you to enjoy the experience in the comfort of our relaxing and inviting shop on Deane Rd, Bolton or opt for the take away option and savour our selection in the comfort of your own home.</p>
-				<p>However it’s not just desserts that we specialise in. As a coffee shop and deli we have something to cater for everybody, from premium espresso based coffees made by trained baristas using locally roasted fresh coffee beans to a selection of hot beverages which will cater for all needs. As to our breakfast and lunch menus, we believe we have all bases covered. All we ask is that you do not deny the disputed fact that you will always have room for dessert.</p>
+				<div class="wrapper flex">
+					<div class="split">
+						<h2>Room Four Dessert</h2>
+						<p>We offer an indulgent range of desserts ranging from the old school custard based desserts for traditionalists to rich and luxurious treats for the dessert aficionado. We also offer freshly made waffles/crepes/pancakes and hot cookie dough served with Movenpick’s finest gourmet ice-cream.</p>
+					</div>
+					<div class="split">
+						<!-- img -->
+					</div>
+				</div>
 			</section>
-			<section class="movenpick">
-				<h2>Movenpick ice cream</h2>
-				<p>We are proud to be official stockists of this luxury gourmet ice cream which is a multiple recipient of the "Cool Brands Award".</p>
-				<p>We serve only Movenpick with all of our desserts, guaranteeing you the very best quality ice cream with every indulgent Room Four Dessert moment</p>
-			</section>
-			<section class="awards">
-				
+			<section class="about-ext hero">
+				<div class="wrapper flex">
+					<div class="split">
+						<h2>A little more about us</h2>
+						<p>We specialise in creating bespoke shot desserts with the sole aim of allowing you to flavour more than one dessert at a time and give you the choice of how much or how little you can enjoy. Quality is of paramount importance to us and we want you to be tantalised by our selection. Room Four Dessert allows you to enjoy the experience in the comfort of our relaxing and inviting shop on Deane Rd, Bolton or opt for the take away option and savour our selection in the comfort of your own home.</p>
+						<p>However it’s not just desserts that we specialise in. As a coffee shop and deli we have something to cater for everybody, from premium espresso based coffees made by trained baristas using locally roasted fresh coffee beans to a selection of hot beverages which will cater for all needs. As to our breakfast and lunch menus, we believe we have all bases covered. All we ask is that you do not deny the disputed fact that you will always have room for dessert.</p>
+					</div>
+				</div>
 			</section>
 		</main>
 
