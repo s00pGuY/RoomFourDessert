06fab7e61d7736ec2e2e7916e46971070cba1ee5
Umesh Patel <umesh.patel@live.co.uk>
2018-02-12 02:44:48 +0000

    updated structure to work with new classes and added new section for the menu to be displayed

--- menu.php
+++ menu.php
@@ -6,8 +6,25 @@ include_once("header.php");
 ?>
 
 		<main id="content" role="main">
-			<section class="intro menus half-scn">
-				<h1>Menu</h1>
+			<section class="intro menus flex hero">
+				<div class="wrapper">
+					<h1>Our menu</h1>
+				</div>
+			</section>
+			<section class="pdf-menu">
+				<div class="wrapper">
+					<h2>Download our menu</h2>
+				</div>
+			</section>
+			<section class="ajax-menu hero">
+				<div class="wrapper">
+					<div class="split">
+						<!-- Title and info -->
+					</div>
+					<div class="split">
+						<!-- Menu -->
+					</div>
+				</div>
 			</section>
 		</main>
 
