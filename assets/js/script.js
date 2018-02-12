85878f08f4d246e0344127c2d77d57bedd00e08b
Umesh Patel <umesh.patel@live.co.uk>
2018-02-12 20:05:15 +0000

    added event listeners via loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 document.querySelector(".hamburger").addEventListener("click", function() {
 	document.getElementById("nav").classList.toggle("nav-open");
 });
+
+var btn = document.getElementsByClassName("sel-menu")[0].querySelectorAll(".btn");
+
+for (var i = 0; i < btn.length; i++) {
+	addEvent(i);
+}
+
+function addEvent(num) {
+	document.getElementById('menu' + num).addEventListener('click', function() {
+		console.log(num);
+	});
+}
