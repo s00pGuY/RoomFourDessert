326b066d5355ed66dfd4f79fc5aa099139628e11
Umesh Patel <umesh.patel@live.co.uk>
2018-02-12 19:38:31 +0000

    added buttons for ajax menu

--- menu.php
+++ menu.php
@@ -17,9 +17,16 @@ include_once("header.php");
 				</div>
 			</section>
 			<section class="ajax-menu hero">
-				<div class="wrapper">
-					<div class="split">
-						<!-- Title and info -->
+				<div class="wrapper flex">
+					<div class="split sel-menu">
+						<h2>View menu</h2>
+						<button class="btn" id="menu0" name="breakfast">Breakfast</button>
+						<button class="btn" id="menu1" name="lunch">Lunch</button>
+						<button class="btn" id="menu2" name="desserts">Desserts</button>
+						<button class="btn" id="menu3" name="ice-cream-sundaes">Ice Cream & Sundaes</button>
+						<button class="btn" id="menu4" name="waffles-crepes-pancakes">Waffles, Crepes & Pancakes</button>
+						<button class="btn" id="menu5" name="cold-drinks">Cold Drinks</button>
+						<button class="btn" id="menu6" name="hot-drinks">Hot Drinks</button>
 					</div>
 					<div class="split">
 						<!-- Menu -->
