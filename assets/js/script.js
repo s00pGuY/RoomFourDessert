e9015a367ee075dda3547ac82858d36e85ea29e8
Umesh Patel <umesh.patel@live.co.uk>
2018-02-13 23:25:05 +0000

    added script to initialse the map

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,28 @@ function addEvent(num) {
 		img.height = img.naturalHeight;
 	});
 }
+
+function initMap() {
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 53.573659, lng: -2.440009},
+		zoom: 17
+	});
+	
+	var infowindow = new google.maps.InfoWindow();
+	var service = new google.maps.places.PlacesService(map);
+	
+	service.getDetails({
+		placeId: 'ChIJJYQkV66ne0gRi9_Kbfplyes'
+	}, function(place, status) {
+		if (status === google.maps.places.PlacesServiceStatus.OK) {
+			var marker = new google.maps.Marker({
+				map: map,
+				position: place.geometry.location
+			});
+			google.maps.event.addListener(marker, 'click', function() {
+				infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + 'Place ID: ' + place.place_id + '<br>' + place.formatted_address + '</div>');
+				infowindow.open(map, this);
+			});
+		}
+	});
+}
